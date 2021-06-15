cdd886b8eb5786d9a6fcc68104720b9e61efc030
abdu-rr-rasheed <abdurrasheed430@gmail.com>
2021-06-15 13:11:34 +0530

    bootstrap5

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-        <link rel="stylesheet" href="./css/app.css">
-        
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script
+            src="https://kit.fontawesome.com/a6518bd95c.js"
+            crossorigin="anonymous"
+        ></script>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+        />
+        <title>E-learning</title>
+        <link rel="stylesheet" href="./css/app.css" />
     </head>
     <body>
-    <div id="app"></div>
-    <script src="./js/app.js"></script>
+        <div id="app"></div>
+        <script src="./js/app.js"></script>
     </body>
 </html>
